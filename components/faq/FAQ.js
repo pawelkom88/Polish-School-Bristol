c30ef7054a6a138368b9f2006218be0e78a3afac
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-20 19:16:08 +0000

    change border color

--- components/faq/FAQ.js
+++ components/faq/FAQ.js
@@ -8,43 +8,43 @@ export default function FAQ() {
         <h4 className="text-4xl font-semibold text-white">
           Najczesciej zadawane pytania
         </h4>
-        <hr className="my-6 dark:border-gray-700" />
+        <hr className="my-6 border-gray-700" />
         <FaqAnswer title="Nasze placowki">
           <p className="font-bold">Prowadzimy 2 placowki w Bristolu: </p>
           <br />
           <p>Bristol Oasis Academy, Bank Leaze BS11 0SN</p>
           <br />
-          <button class="dark:bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button class="bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Pokaz na mapie
           </button>
           <br />
           <p>Holy Cross Primary School, Dean Ln, Bedminster, BS3 1DB,</p>
           <br />
-          <button class="dark:bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button class="bg-gray-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Pokaz na mapie
           </button>
           <br />
         </FaqAnswer>
-        <hr className="my-8 border-gray-200 dark:border-gray-700" />
+        <hr className="my-8  border-gray-700" />
         <FaqAnswer title="Question !">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum
           quae. Harum officiis reprehenderit ex quia ducimus minima id provident
           molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.
         </FaqAnswer>
 
-        <hr className="my-8 border-gray-200 dark:border-gray-700" />
+        <hr className="my-8  border-gray-700" />
         <FaqAnswer title="Question !">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum
           quae. Harum officiis reprehenderit ex quia ducimus minima id provident
           molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.
         </FaqAnswer>
-        <hr className="my-8 border-gray-200 dark:border-gray-700" />
+        <hr className="my-8  border-gray-700" />
         <FaqAnswer title="Question !">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, eum
           quae. Harum officiis reprehenderit ex quia ducimus minima id provident
           molestias optio nam vel, quidem iure voluptatem, repellat et ipsa.
         </FaqAnswer>
-        <hr className="my-8 border-gray-200 dark:border-gray-700" />
+        <hr className="my-8  border-gray-700" />
       </div>
     </section>
   )
