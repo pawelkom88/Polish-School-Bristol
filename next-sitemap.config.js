98e3721e4a0f46247e1887581a567fa72d7db2ca
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-21 13:01:30 +0000

    add SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://joe-doe-photography.netlify.app/',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
---
+++ seo.config.js
@@ -0,0 +1,16 @@
+
+export const favicon = [
+  {
+    rel: 'icon',
+    href: '/favicon-32x32.png',
+  },
+  {
+    rel: 'apple-touch-icon',
+    href: '/apple-touch-icon.png',
+    sizes: '76x76',
+  },
+  {
+    rel: 'manifest',
+    href: '/site.webmanifest',
+  },
+]
