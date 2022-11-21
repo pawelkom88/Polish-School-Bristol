cf26ab529464fecc3c1a42f91676b71c7e9eed5c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-21 13:01:07 +0000

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'primary-clr': 'var(--bg-clr)',
+      },
+    },
+  },
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
