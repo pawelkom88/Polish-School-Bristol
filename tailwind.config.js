d1eaf0947e3f9f7d9242e177e5b69c34b2bf93b6
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2022-11-24 11:53:46 +0000

    edit tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   theme: {
+    purge: ['./**/*.{html,js,jsx}'],
+    safelist: ['primary-clr', 'btn1-clr', 'btn2-clr', 'bg1-clr', 'bg2-clr'],
+
     extend: {
       colors: {
         'primary-clr': 'var(--bg-clr)',
         'btn1-clr': 'var(--btn1-clr)',
         'btn2-clr': 'var(--btn2-clr)',
+        'bg1-clr': 'var(--bg1-clr)',
+        'bg2-clr': 'var(--bg2-clr)',
+      },
+      animation: {
+        'spin-slow': 'spin 12s linear infinite',
       },
     },
   },
@@ -13,4 +21,4 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-}
+};
