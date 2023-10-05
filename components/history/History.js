af04194e5120732f0f42b5acc5a4157a83ecd814
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2023-10-05 13:40:41 +0100

    history update

--- components/history/History.js
+++ components/history/History.js
@@ -3,16 +3,16 @@ import React from 'react';
 export default function History() {
   return (
     <p className="text-lg sm:text-center indent-3.5">
-      Szkoła powstała w 2018, aby wspierać liczną polską społeczność w
-      południowej – zachodniej Anglii i Walii. Oryginalnie nasza szkoła została
-      założona w Newport jako Polska Szkoła im. Armii Andersa w Newport CIC.
-      Niestety pandemia COVID-19 zmusiła nas do przejścia w nauczanie zdalne i
-      ostatecznie przeniesienie szkoły na teren miasta Bristol. Pomimo
-      przeciwności losu zdobyliśmy zaufanie uczniów, rodziców, lokalnych władz i
-      organizacji społecznych. Obecnie szkoła funkcjonuje pod nazwą Polska
-      Szkoła Języka i Kultury z dwoma oddziałami w Holy Cross Catholic Primary
-      School i Oasis Academy Bank Leaze. Nasza społeczność szkolna liczy blisko
-      250 osób!
+      Szkoła została założona w 2018 roku, aby wspierać licznie reprezentowaną
+      polską społeczność na południowo-zachodniej Anglii i w Walii. Pierwotnie
+      nasza szkoła działała w Newport pod nazwą "Polska Szkoła im. Armii Andersa
+      w Newport CIC". Niestety pandemia COVID-19 zmusiła nas do przejścia na
+      nauczanie zdalne i ostatecznie przeniesienia szkoły do miasta Bristol.
+      Mimo trudności zdobyliśmy zaufanie uczniów, rodziców, lokalnych władz i
+      organizacji społecznych. Obecnie nasza szkoła funkcjonuje jako "Polska
+      Szkoła Języka i Kultury" z dwoma oddziałami w Holy Cross Catholic Primary
+      School i Oasis Academy Bank Leaze. Nasza społeczność szkolna liczy obecnie
+      blisko 250 osób!
     </p>
   );
 }
