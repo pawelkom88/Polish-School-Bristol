ff0b199457485838734753dd57f1f94bdf12d394
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2023-11-29 18:11:38 +0000

    changes

--- components/history/History.js
+++ components/history/History.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function History() {
   return (
-    <p className="text-lg sm:text-center indent-3.5">
+    <p className="text-lg sm:text-center indent-3.5 text-justify">
       Szkoła została założona w 2018 roku, aby wspierać licznie reprezentowaną
       polską społeczność na południowo-zachodniej Anglii i w Walii. Pierwotnie
       nasza szkoła działała w Newport pod nazwą "Polska Szkoła im. Armii Andersa
--- components/why-us/WhyUs.js
+++ components/why-us/WhyUs.js
@@ -1,9 +1,10 @@
-import { cardData } from '@data/cardData';
 import Card from '@components/card/Card';
+import { cardData } from '@data/cardData';
+import Image from 'next/image';
 
 export default function WhyUs() {
   return (
-    <div className="w-full min-h-[50rem] lg:min-h-[40rem] bg-white md:bg-[linear-gradient(to_right_bottom,rgba(255,255,255,0.8),rgba(255,255,255,0.8)),url('/why-usoverlay.webp')] bg-cover md:bg-auto flex flex-col justify-start items-center gap-4">
+    <div className="flex flex-col justify-start items-center gap-4">
       <h2 className="xl:text-5xl text-4xl text-center font-bold mt-16 mb-8 ">
         Dlaczego my ?
       </h2>
@@ -12,6 +13,51 @@ export default function WhyUs() {
           return <Card key={card.id} card={card} />;
         })}
       </div>
+      <div className="max-w-7xl my-12 px-4">
+        <h2 class="xl:text-5xl text-4xl text-center font-bold my-7">
+          Wiadomości
+        </h2>
+        <Image src="/funds.webp" width={1200} height={766} alt="funds" />
+        <br />
+        <br />
+        <p class="text-lg text-justify">
+          Informujemy, że nasza szkoła otrzymała dofinansowanie przez Instytut
+          Rozwoju Języka Polskiego im. Świętego Maksymiliana Marii Kolbego ze
+          środków Ministra Edukacji i Nauki w ramach konkursu "Wspieranie
+          Edukacji Polonijnej i Polskiej Poza Granicami Polski 2023”.
+        </p>
+        <br />
+        <p class="text-lg text-justify">
+          Nazwa przedsięwzięcia: Szkoła (bardziej) polska. Wsparcie oświaty
+          polonijnej w okręgu konsularnym Konsula RP w Manchesterze (I)
+        </p>
+        <br />
+        <ul class="text-lg">
+          Kwota dotacji:
+          <li className="list-disc">
+            w 2023 r.: <strong>29 483,00 PLN</strong>
+          </li>
+          <li className="list-disc">
+            w 2024 r.: <strong>35 468,00 PLN</strong>
+          </li>
+        </ul>
+        <br />
+        <p class="text-lg">
+          Całkowita wartość przedsięwzięcia: <strong>811 400,00 PL</strong>N
+        </p>
+        <br />
+        <p class="text-lg text-justify">
+          Przedsięwzięcie{' '}
+          <strong>
+            „Szkoła (bardziej) polska. Wsparcie oświaty polonijnej w okręgu
+            konsularnym Konsula RP w Manchesterze (I)”
+          </strong>{' '}
+          polega na wsparciu szkół polonijnych poprzez dofinansowanie kosztów
+          wynajmu pomieszczeń, wynagrodzeń nauczycieli, ubezpieczenia i innych
+          kosztów funkcjonowania.
+        </p>
+      </div>
+      <div className="w-full min-h-[0rem] lg:min-h-[40rem] md:bg-auto  bg-white md:bg-[linear-gradient(to_right_bottom,rgba(255,255,255,0.8),rgba(255,255,255,0.8)),url('/why-usoverlay.webp')] bg-cover"></div>
     </div>
   );
 }
